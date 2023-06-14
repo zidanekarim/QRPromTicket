c6b59238eea5bc1ecb67e021ef95063c3b07b8ec
Zidane Karim <zkarim7676@gmail.com>
2023-06-13 21:51:28 -0400

    newlines breaking

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", function(req, res)  {
     res.sendFile(__dirname + "/public/index.html");
 });
-const result = [];
+var result = [];
 
 
 
 app.post("/run", async (req, res) => {
-
+  result = []; 
 
   try {
 
@@ -27,7 +27,7 @@ app.post("/run", async (req, res) => {
     ))
     .on("data", (data) => {
         if (data[0].length > 5) {
-            console.log("data: ", data);
+           // console.log("data: ", data);
             if (data[0].includes(",") && data[0].split(",")[1].length > 0) {
                 result.push([data[0], data[0].split(",")[1]]);
             }
@@ -48,10 +48,10 @@ app.post("/run", async (req, res) => {
         }
     })
     .on("end", () => {
-                //console.log(result);
+                console.log(result);
 
                 // Overwrite the CSV file
-                const csvData = result.map(row => row.join(",")).join("\n");
+                const csvData = result.map(row => row.join(",")).join('\n');
                 
                 fs.writeFile("./emails.csv", csvData, (err) => {
                     if (err) throw err;
